e6b35fc386adcb7285566473d5fcf0acbb1ee666
Frederico Winter <frdw@protonmail.com>
2022-12-29 18:25:30 -0300

    added new options

--- nvim/lua/settings/init.lua
+++ nvim/lua/settings/init.lua
@@ -2,15 +2,17 @@ local set = vim.opt
 local autocmd = vim.api.nvim_create_autocmd
 
 set.number = true
+set.numberwidth = 4
 set.wildmenu = true
 set.ignorecase = true
+set.hlsearch = true
 set.hidden = true
 set.backup = false
 set.writebackup = false
 set.encoding = 'utf-8'
 set.fileencoding = 'utf-8'
 set.pumheight = 10
--- set.cmdheight = 2
+set.cmdheight = 2
 set.expandtab = false
 set.shiftwidth = 4
 set.softtabstop = 4
@@ -18,6 +20,7 @@ set.tabstop = 4
 set.splitbelow = true
 set.splitright = true
 set.smartindent = true
+set.smartcase = true
 set.autoindent = true
 set.laststatus = 0
 set.cursorline = true
@@ -30,6 +33,12 @@ set.mouse = 'nv'
 set.foldcolumn = '0'
 set.signcolumn = 'no'
 set.termguicolors = true
+set.clipboard = 'unnamedplus'
+set.showtabline = 2
+set.swapfile = false
+set.undofile = true
+set.scrolloff = 8
+set.sidescrolloff = 8
 
 -- Autoload nvim lua config file on save
 --autocmd('BufWritePost', {
