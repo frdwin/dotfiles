619034f7505690986313abe24055abc1426c1e73
Frederico Winter <frdw@protonmail.com>
2022-12-30 17:46:46 -0300

    added ident on visual mode

--- nvim/lua/user/mappings.lua
+++ nvim/lua/user/mappings.lua
@@ -3,43 +3,47 @@ local silent_opts = { noremap = true, silent = true }
 local opts = { noremap = true }
 
 -- Leader key
-map('n', '<Space>', '', {})
-vim.g.mapleader = ' '
+map("n", "<Space>", "", {})
+vim.g.mapleader = " "
 
 -- General mappings
-map('i', 'jk', '<Esc>', opts)
-map('n', ';', ':', opts)
-map('v', ';', ':', opts)
+map("i", "jk", "<Esc>", opts)
+map("n", ";", ":", opts)
+map("v", ";", ":", opts)
 
 -- Save on Ctrl+S
-map('n', '<C-s>', ':w<CR>', opts)
+map("n", "<C-s>", ":Format<CR> :w<CR>", opts)
 
 -- Navigation
-map('n', '<C-j>', '<C-w>j', opts)
-map('n', '<C-h>', '<C-w>h', opts)
-map('n', '<C-k>', '<C-w>k', opts)
-map('n', '<C-l>', '<C-w>l', opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
 
-map('n', '<up>', ':resize +2<CR>', opts)
-map('n', '<down>', ':resize -2<CR>', opts)
-map('n', '<left>', ':vertical resize -2<CR>', opts)
-map('n', '<right>', ':vertical resize +2<CR>', opts)
+map("n", "<up>", ":resize +2<CR>", opts)
+map("n", "<down>", ":resize -2<CR>", opts)
+map("n", "<left>", ":vertical resize -2<CR>", opts)
+map("n", "<right>", ":vertical resize +2<CR>", opts)
 
-map('n', '<tab>', ':bn<CR>', opts)
-map('n', '<s-tab>', ':bp<CR>', opts)
-map('n', '<leader>q', ':bd<CR>', opts)
+map("n", "<tab>", ":bn<CR>", opts)
+map("n", "<s-tab>", ":bp<CR>", opts)
+map("n", "<leader>q", ":bd<CR>", opts)
 
-map('n', '<leader>e', ':NvimTreeToggle<CR>', opts)
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Indent on visual mode
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
 
 -- Move text up and down on visual mode
-map('x', 'J', ":move '>+1<CR>gv-gv", opts)
-map('x', 'K', ":move '<-2<CR>gv-gv", opts) 
+map("x", "J", ":move '>+1<CR>gv-gv", opts)
+map("x", "K", ":move '<-2<CR>gv-gv", opts)
 
 -- Cancel search highlight
-map('n', '<leader>h', ':nohl<CR>', opts)
+map("n", "<leader>h", ":nohl<CR>", opts)
 -- Paste from last copy
-map('n', ',p', '"0p', opts)
-map('n', ',P', '"0P', opts)
+map("n", ",p", '"0p', opts)
+map("n", ",P", '"0P', opts)
 
 -- Startify
-map('n', '<leader>s', ':Startify<CR>', opts)
+map("n", "<leader>s", ":Startify<CR>", opts)
