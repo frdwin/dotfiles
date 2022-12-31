250bb48523d7f17923dc26b6ec8ca23ef4879090
Frederico Winter <frdw@protonmail.com>
2022-12-31 09:40:49 -0300

    fixed formatting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,10 @@
-require "user.mappings"
-require "user.settings"
-require "user.plugins"
-require "user.plugins_config"
-require "user.lsp"
-require "user.colorscheme"
+-- TODO:
+-- configure each lsp
+-- add alpha
+-- add whichkey?
+require("user.mappings")
+require("user.settings")
+require("user.plugins")
+require("user.plugins_config")
+require("user.lsp")
+require("user.colorscheme")
